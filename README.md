b719f22a3d88fa46ade6836fce2f0156cd94504b
IMOKURI <nenegi.01mo@gmail.com>
2019-12-17 11:28:58 +0900

    first step

---
+++ README.md
@@ -0,0 +1,12 @@
+# BERT examples
+
+[BERTを理解しながら自分のツイートを可視化してみるハンズオン](https://qiita.com/yuki_uchida/items/09fda4c5c608a9f53d2f)をやってみた。
+
+## Prerequisites
+
+- `pip install --user torch torchvision transformers scikit-learn`
+- download `通常版: Japanese_L-12_H-768_A-12_E-30_BPE_transformers.zip (393M; 19/11/15公開)` from [BERT日本語Pretrainedモデル](http://nlp.ist.i.kyoto-u.ac.jp/index.php?BERT%E6%97%A5%E6%9C%AC%E8%AA%9EPretrained%E3%83%A2%E3%83%87%E3%83%AB)
+
+## Run
+
+`./main.py`
---
+++ main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# pylint: disable=invalid-name
+
+import numpy as np
+
+import torch
+
+from transformers import BertForMaskedLM, BertTokenizer
+
+text = "How many airports are there in Japan?"
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+tokenized_text = tokenizer.tokenize(text)
+tokenized_text.insert(0, "[CLS]")
+tokenized_text.append("[SEP]")
+
+print(tokenized_text)
+# ['[CLS]', 'how', 'many', 'airports', 'are', 'there', 'in', 'japan', '?', '[SEP]']
+
+masked_index = 4
+tokenized_text[masked_index] = "[MASK]"
+
+print(tokenized_text)
+# ['[CLS]', 'how', 'many', 'airports', '[MASK]', 'there', 'in', 'japan', '?', '[SEP]']
+
+# テキストのままBERTに渡すのではなく、辞書で変換し、idになった状態にする。
+tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+tokens_tensor = torch.tensor([tokens])
+
+model = BertForMaskedLM.from_pretrained("bert-base-uncased")
+model.eval()
+
+with torch.no_grad():
+    outputs = model(tokens_tensor)
+    predictions = outputs[0]
+
+# masked_indexとなっている部分の単語の予測結果を取り出し、その予測結果top5を出す
+_, predict_indexes = torch.topk(predictions[0, masked_index], k=5)
+predict_tokens = tokenizer.convert_ids_to_tokens(predict_indexes.tolist())
+
+print(predict_tokens)
+# ['are', 'were', 'out', 'is', 'go']
+
+# TODO: 以下の予測を試してみたい
+# 1. BertForNextSentencePrediction
+# 2. BertForSequenceClassification
+# 3. BertForTokenClassification
+# 4. BertForQuestionAnswering
